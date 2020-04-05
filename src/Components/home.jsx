9b807ccdc655e88b1ec7c389e2d4d5def8b97f66
xian <xian@xians-Mac.local>
2020-04-05 20:47:34 +0200

    Change Home component to be more responsive

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -3,48 +3,53 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="description">
-      <h1>Full Stack Developer</h1>
-      <p>
-        Offering creative Backend and Frontend Web/Mobile solutions to business
-        and technology challenges and provide digital marketing services as
-        well.
-      </p>
-      <Link className="btn btn-primary btn-lg" to="/about">
-        See more
-      </Link>
-
-      <br />
-      <h1 className="text-center">Portofolio</h1>
-      <h6 className="text-center">
-        <Link to="/portofolio">See more</Link>
-      </h6>
-
+    <div className="container">
       <div className="row">
-        <div className="col-sm">
-          <div className="feature_item">
-            <h4>
-              ATA (Agent to Agent Transfer Balance in Mobile Financial Services)
-            </h4>
-            The application enables the agents on 2 differents Mobile Financial
-            Services to share their balance between the two platforms and
-            provide quick services to the customers.
-          </div>
+        <div className="col-sm-7 home">
+          <h1>Full Stack Developer</h1>
+          <p>
+            Offering creative Backend and Frontend Web/Mobile solutions to
+            business and technology challenges and provide digital marketing
+            services as well.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/about">
+            See more
+          </Link>
         </div>
-        <div className="col-md-4 col-lg-4 col-sm-12">
-          <div className="feature_item">
-            <h4>PAM(Purchase Airtime with Mobile Financial Services)</h4>
-            The application enables the dealers to purchase Airtime on
-            Electronic Recharge platform with balance on Mobile Financial
-            Service wallet.
+        <div className="col-sm-5 home">
+          <h1>Portofolio</h1> <Link to="/portofolio">See more</Link>
+          <div className="row">
+            <div className="col-sm">
+              <div className="feature_item">
+                <h4>
+                  ATA ( Agent to Agent Transfer Balance on Mobile Financial
+                  Services )
+                </h4>
+                The application enables the agents on two differents Mobile
+                Financial Services to share their balance between the two
+                platforms and provide quick services to the customers.
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="col-md-4 col-lg-4 col-sm-12">
-          <div className="feature_item">
-            <h4>ScratchCards Management System</h4>
-            Web Application that manages Stock and purchase of Prepaid Voucher
-            Top-up Cards in all Shops with dealers and activation on Voucher
-            system.
+          <div className="row">
+            <div className="col-sm">
+              <div className="feature_item">
+                <h4>PAM ( Purchase Airtime with Mobile Financial Services )</h4>
+                The application enables the dealers to purchase Airtime on
+                Electronic Recharge platform with balance on Mobile Financial
+                Service wallet.
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm">
+              <div className="feature_item">
+                <h4>ScratchCards Management System</h4>
+                Web Application that manages Stock and purchase of Prepaid
+                Voucher Top-up Cards in all Shops with dealers and activation on
+                Voucher system.
+              </div>
+            </div>
           </div>
         </div>
       </div>
